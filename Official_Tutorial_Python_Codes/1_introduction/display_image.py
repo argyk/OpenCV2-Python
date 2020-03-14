b502c66a8ea8939234d47af865e6abd55d524e7f
argyk <arg.katsogr@hotmail.com>
2020-03-14 20:21:23 +0100

    Ran first program
    Corrected first sample program from introduction

--- Official_Tutorial_Python_Codes/1_introduction/display_image.py
+++ Official_Tutorial_Python_Codes/1_introduction/display_image.py
@@ -17,16 +17,16 @@ import sys
 import numpy as np
 
 if len(sys.argv)!=2:                  ## Check for error in usage syntax
-    print "Usage : python display_image.py <image_file>"
+    print("Usage : python display_image.py <image_file>")
 
 else:
-    img = cv2.imread(sys.argv[1],cv2.CV_LOAD_IMAGE_COLOR)  ## Read image file
+    img = cv2.imread(sys.argv[1])  ## Read image file
 
-    if (img == None):                      ## Check for invalid input
-        print "Could not open or find the image"
+    if (img == []):                      ## Check for invalid input
+        print("Could not open or find the image")
     else:
         cv2.namedWindow('Display Window')        ## create window for display
         cv2.imshow('Display Window',img)         ## Show image in the window
-        print "size of image: ",img.shape        ## print size of image
+        print("size of image: %s" % str(img.shape))       ## print size of image
         cv2.waitKey(0)                           ## Wait for keystroke
         cv2.destroyAllWindows()                  ## Destroy all windows
