fb59cf697c5aaa1cb98993b6f6b34cd2346f0455
argyk <arg.katsogr@hotmail.com>
2020-03-14 20:23:32 +0100

    Ran second program
    Corrected and ran modify_image script

--- Official_Tutorial_Python_Codes/1_introduction/modify_image.py
+++ Official_Tutorial_Python_Codes/1_introduction/modify_image.py
@@ -16,7 +16,7 @@ import cv2
 import numpy as np
 import sys
 
-image = cv2.imread('lena.jpg') # change image name as you need or give sys.argv[1] to read from command line
+image = cv2.imread(sys.argv[1]) # change image name as you need or give sys.argv[1] to read from command line
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert image to gray
 
 cv2.imwrite('gray_image.jpg',gray_image)   # saves gray image to disk
